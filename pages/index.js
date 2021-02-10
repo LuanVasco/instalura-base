12e225c6b7b22dd87437ec851d7508f6e3f4cb9b
LuanVasco <devluangodoy@gmail.com>
2021-02-10 14:36:24 -0300

    Código da aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
-import styled from 'styled-components'
-
-/* const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-` */
-
-function Title(props) {
-  return <h1>{props.children}</h1>
-}
+import Menu from './../src/components/commons/Menu';
 
 export default function Home() {
   return (
-    <div>
-      <Title>
-        Olá Mundo!
-      </Title>
-      Hello World
+    <div> 
+      <Menu />
+      
     </div>
-  );
+  )
 }
