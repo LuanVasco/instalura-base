7e92bac206676e2c0c14a3cf12515f117f1c7517
LuanVasco <devluangodoy@gmail.com>
2021-02-12 19:17:15 -0300

    Atualização aula 09

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,12 +1,9 @@
-// eslint-disable-next-line linebreak-style
 import React from 'react';
-import Menu from '../src/components/commons/Menu';
-import Box from '../src/components/foundation/Layout/Box';
 
 export default function PageSobre() {
   return (
-    <Box>
-      <Menu />
-    </Box>
+    <div>
+      Página Sobre
+    </div>
   );
 }
